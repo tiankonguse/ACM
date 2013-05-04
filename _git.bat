eac5e5e46c7a4b6e5f4346940231b1d6eb34aa45
tiankongever <shen10000shen@gmail.com>
2013-05-04 10:58:32 +0800

    20130504105832

--- _git.bat
+++ _git.bat
@@ -1,6 +1,4 @@
 @git add -A
-@echo «Î ‰»Îcommit¿Ì”…
-@set /p a=
-@git commit -m "%a%"
+@git commit -m "%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"
 @git push origin  master
 @pause
