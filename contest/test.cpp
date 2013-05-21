f57e1796bb0790eef247dfa1d29ed888537dece5
tiankongever <shen10000shen@gmail.com>
2013-05-21 13:27:59 +0800

    20130521132758

--- contest/test.cpp
+++ contest/test.cpp
@@ -16,35 +16,13 @@
 #include<functional>
 #include<algorithm>
 using namespace std;
-
-struct Point_2d {
-    double x,y;
-};
-struct Point_3d {
-    double x,y,z;
-};
-
-double distance_2d(Point_2d const&a,Point_2d const&b) {
-    return(sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)));
+int id(int l,int r) {
+    return l+r | l!=r;
 }
-double distance_3d(Point_3d &a, Point_3d&b) {
-    return(sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)+(a.z-b.z)*(a.z-b.z)));
-}
-double triangleArea(Point_2d const&a,Point_2d const&b,Point_2d const&c) {
-    double ab,bc,ca,p,s;
-    ab=distance_2d(a,b);
-    bc=distance_2d(b,c);
-    ca=distance_2d(c,a);
-    p=(ab+bc+ca)/2;
-    s=sqrt(p*(p-ab)*(p-bc)*(p-ca));
-    return s;
-}
-
-
-
-
-
 int main() {
-
+    int a,b;
+    while(~scanf("%d%d",&a,&b)){
+        printf("%d\n",id(a,b));
+    }
     return 0;
 }
--- 整理/划分树/划分树02.cpp
+++ 整理/划分树/划分树02.cpp
@@ -16,7 +16,7 @@ void build(int l,int r,int d) {
     if(l==r) return;
     int mid=(l+r)>>1,p=0;
     for(int i=l; i<=r; i++) {
-        if(val[d][i]<=val[d][mid]) {
+        if(val[d][i]<=mid) {
             val[d+1][l+p]=val[d][i];
             lfnum[d][i]=++p;
         } else {
@@ -39,11 +39,23 @@ int query(int s,int e,int k,int l=1,int r=n,int d=0) {
 int main() {
     int cas=0,m,l,r,k;
     while(scanf("%d%d",&n,&m)!=EOF) {
-        for(int i=1; i<=n; i++) scanf("%d",arr+i),sortedPos[i]=i;
+        for(int i=1; i<=n; i++){
+            scanf("%d",arr+i),sortedPos[i]=i;
+        }
         sort(sortedPos+1,sortedPos+n+1,cmp);
         for(int i=1; i<=n; i++){
             val[0][sortedPos[i]]=i;
         }
+
+        for(int i=1;i<=n;i++){
+            printf("%d ",sortedPos[i]);
+        }
+        puts("");
+        for(int i=1;i<=n;i++){
+            printf("%d ",val[0][i]);
+        }
+        puts("");
+
         build(1,n,0);
         while(m--) {
             scanf("%d%d%d",&l,&r,&k);
@@ -51,3 +63,12 @@ int main() {
         }
     }
 }
+/*
+7 4
+1 6 2  3 9 3 1
+
+7 4
+1 6 2  3 9 3 1  // input
+1 7 3 4 6 2 5   //sortedPos input的第几个位置的值应该在当前位置
+1 6 3 4 7 5 2  //val  input的当前值应该在那个位置
+*/
