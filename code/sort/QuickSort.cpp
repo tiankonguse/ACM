7a6052bcadd003da67e6c6985f064365a18e972c
tiankonguse <i@tiankonguse.com>
2018-09-17 15:24:58 +0800

    update

--- code/sort/QuickSort.cpp
+++ code/sort/QuickSort.cpp
@@ -9,6 +9,7 @@
 
 #include<iostream>
 #include<cstdio>
+#include<time.h>
 #include<cstring>
 #include<cstdlib>
 #include<string>
@@ -26,56 +27,62 @@ typedef __int64 LL;
 typedef long long LL;
 #endif
 void swap(int &first, int& second) {
-	int temp = first;
-	first = second;
-	second = temp;
+    int temp = first;
+    first = second;
+    second = temp;
 }
 void QuickSort(int len, int* array) {
-	if(len <= 1){
-		return ;
-	}
-	int leftStart = 1; //[0,leftStart),
-	int rightEnd = len-1; //(rightEnd, len)
+    if(len <= 1) {
+        return ;
+    }
+    swap(array[0], array[rand() % len]);
 
-	while(leftStart < rightEnd){
-		if(array[leftStart] > array[0]){
-			swap(array[leftStart], array[rightEnd]);
-			rightEnd--; //之后的都大于array[0]
-		}else{
-			leftStart++;//之前的都小于等于array[0]
-		}
-	}
-	//这个时候 leftStart = rightEnd，这个位置进行特殊判断
-	if(array[leftStart] > array[0]){
-		leftStart--;
-		swap(array[leftStart], array[0]);
-	}else{
-		swap(array[leftStart], array[0]);
-	}
+    int leftStart = 1; //[0,leftStart),
+    int rightEnd = len-1; //(rightEnd, len)
 
-	QuickSort(leftStart,  array);//[0, leftStart)
-	QuickSort(len - leftStart - 1, array + leftStart + 1);
+    while(leftStart < rightEnd) {
+        if(array[leftStart] > array[0]) {
+            swap(array[leftStart], array[rightEnd]);
+            rightEnd--; //之后的都大于array[0]
+        } else {
+            leftStart++;//之前的都小于等于array[0]
+        }
+    }
+    //这个时候 leftStart = rightEnd，这个位置进行特殊判断
+    if(array[leftStart] > array[0]) {
+        leftStart--;
+        swap(array[leftStart], array[0]);
+    } else {
+        swap(array[leftStart], array[0]);
+    }
+
+    QuickSort(leftStart,  array);//[0, leftStart)
+    QuickSort(len - leftStart - 1, array + leftStart + 1);
+}
+
+void test(int n) {
+    int array[10];
+    int tmparray[10];
+    for (int i = 0; i < n; i++) {
+        array[i] = rand()%100;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+    QuickSort(n, array);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
 
 int main() {
-	int array[10];
-	int tmparray[10];
-
-	for (int i = 0; i < 10; i++) {
-		array[i] = 10 - i;
-	}
-
-	int n = 8;
-
-	for (int i = 0; i < n; i++) {
-		printf("%d ", array[i]);
-	}
-	printf("\n");
-	QuickSort(n, array);
-	for (int i = 0; i < n; i++) {
-		printf("%d ", array[i]);
-	}
-	printf("\n");
-
-	return 0;
+    srand(time(NULL));
+    for(int i=1; i<=10; i++) {
+        printf("test case: %d\n", i);
+        test(i);
+    }
+    return 0;
 }
