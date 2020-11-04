16d7ab32db65cd6c91e519da63eaae7950cbf545
tiankongsue <i@tiankonguse.com>
2020-11-04 14:51:15 +0800

    update

--- tmpleate-2020/code/discreteArray.cc
+++ tmpleate-2020/code/discreteArray.cc
@@ -19,67 +19,53 @@ const double PI = acos(-1.0), eps = 1e-7;
 const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
 const int max3 = 2100, max4 = 11100, max5 = 55000, max6 = 2000100;
 
-class DataOffline {
+class DiscreteArray {
  public:
- void Init(int n){
-     m.reserve(n);
-     vec.reserve(n);
- }
-  void Add(ll v) {
-    if (m.count(v) == 0) {
-      m[v] = 0;
-      vec.push_back(v);
-    }
-  }
+  void Add(ll v) { m[v] = 0; }
   void Build() {
-    index = vec.size();
-    sort(vec.begin(), vec.end());
-    for (int i = 0; i < vec.size(); i++) {
-      m[vec[i]] = i + 1;
+    index = 0;
+    for (auto& p : m) {
+      p.second = ++index;
     }
   }
   int Get(ll v) { return m[v]; }
   int Size() { return index; }
   int Lower(ll v) {
-    auto it = lower_bound(vec.begin(), vec.end(), v);
-    if (it == vec.end()) {
+    auto it = m.lower_bound(v);
+    if (it == m.end()) {
       return index + 1;
     }
-    return m[*it];
+    return it->second;
   }
   int Upper(ll v) {
-    auto it = upper_bound(vec.begin(), vec.end(), v);
-    if (it == vec.end()) {
+    auto it = m.upper_bound(v);
+    if (it == m.end()) {
       return index + 1;
     }
-    return m[*it];
+    return it->second;
   }
-  void Reset() {
-    m.clear();
-    vec.clear();
-  }
+  void Reset() { m.clear(); }
 
  private:
   int index;
-  unordered_map<ll, int> m;
-  vector<ll> vec;
+  map<ll, int> m;
 };
 
 int main() {
-  DataOffline dataOffline;
-  dataOffline.Reset();
+  DiscreteArray discreteArray;
+  discreteArray.Reset();
   for (int i = 0; i < 10; i++) {
-    dataOffline.Add(i * i);
+    discreteArray.Add(i * i);
   }
-  dataOffline.Build();
+  discreteArray.Build();
   for (int i = 0; i < 10; i++) {
-    printf("v=%d index=%d\n", i * i, dataOffline.Get(i * i));
-  }
-  for (int i = 0; i < 10; i++) {
-    printf("v=%d Lower=%d\n", i, dataOffline.Lower(i));
+    printf("v=%d index=%d\n", i * i, discreteArray.Get(i * i));
   }
   for (int i = 0; i < 10; i++) {
-    printf("v=%d Upper=%d\n", i, dataOffline.Upper(i));
+    printf("v=%d Lower=%d\n", i, discreteArray.Lower(i));
+  }
+  for (int i = 0; i < 10; i++) {
+    printf("v=%d Upper=%d\n", i, discreteArray.Upper(i));
   }
 
   return 0;
--- tmpleate-2020/code/discreteArrayEx.cc
+++ tmpleate-2020/code/discreteArrayEx.cc
@@ -19,53 +19,67 @@ const double PI = acos(-1.0), eps = 1e-7;
 const int inf = 0x3f3f3f3f, ninf = 0xc0c0c0c0, mod = 1000000007;
 const int max3 = 2100, max4 = 11100, max5 = 55000, max6 = 2000100;
 
-class DataOffline {
+class DiscreteArray {
  public:
-  void Add(ll v) { m[v] = 0; }
+ void Init(int n){
+     m.reserve(n);
+     vec.reserve(n);
+ }
+  void Add(ll v) {
+    if (m.count(v) == 0) {
+      m[v] = 0;
+      vec.push_back(v);
+    }
+  }
   void Build() {
-    index = 0;
-    for (auto& p : m) {
-      p.second = ++index;
+    index = vec.size();
+    sort(vec.begin(), vec.end());
+    for (int i = 0; i < vec.size(); i++) {
+      m[vec[i]] = i + 1;
     }
   }
   int Get(ll v) { return m[v]; }
   int Size() { return index; }
   int Lower(ll v) {
-    auto it = m.lower_bound(v);
-    if (it == m.end()) {
+    auto it = lower_bound(vec.begin(), vec.end(), v);
+    if (it == vec.end()) {
       return index + 1;
     }
-    return it->second;
+    return m[*it];
   }
   int Upper(ll v) {
-    auto it = m.upper_bound(v);
-    if (it == m.end()) {
+    auto it = upper_bound(vec.begin(), vec.end(), v);
+    if (it == vec.end()) {
       return index + 1;
     }
-    return it->second;
+    return m[*it];
   }
-  void Reset() { m.clear(); }
+  void Reset() {
+    m.clear();
+    vec.clear();
+  }
 
  private:
   int index;
-  map<ll, int> m;
+  unordered_map<ll, int> m;
+  vector<ll> vec;
 };
 
 int main() {
-  DataOffline dataOffline;
-  dataOffline.Reset();
+  DiscreteArray discreteArray;
+  discreteArray.Reset();
   for (int i = 0; i < 10; i++) {
-    dataOffline.Add(i * i);
+    discreteArray.Add(i * i);
   }
-  dataOffline.Build();
+  discreteArray.Build();
   for (int i = 0; i < 10; i++) {
-    printf("v=%d index=%d\n", i * i, dataOffline.Get(i * i));
-  }
-  for (int i = 0; i < 10; i++) {
-    printf("v=%d Lower=%d\n", i, dataOffline.Lower(i));
+    printf("v=%d index=%d\n", i * i, discreteArray.Get(i * i));
   }
   for (int i = 0; i < 10; i++) {
-    printf("v=%d Upper=%d\n", i, dataOffline.Upper(i));
+    printf("v=%d Lower=%d\n", i, discreteArray.Lower(i));
+  }
+  for (int i = 0; i < 10; i++) {
+    printf("v=%d Upper=%d\n", i, discreteArray.Upper(i));
   }
 
   return 0;
